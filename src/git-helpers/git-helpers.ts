0088afb23d85d5480ac7252801fa2911c595a1da
Parmvir-S <parmvir.shergill@gmail.com>
2024-07-24 20:28:12 -0700

    worked on processing git status log

--- src/git-helpers/git-helpers.ts
+++ src/git-helpers/git-helpers.ts
@@ -17,7 +17,7 @@ export async function setupRepository(): Promise<void> {
     try {
         const { stdout, stderr } = await exec('git init');
         console.log(stdout);
-        if (!remoteOriginSet) {
+if (!remoteOriginSet) {
             await exec(`git remote add origin ${process.env.GIT_REPO}`);
             await exec('git branch -M main');
             await exec('git add .');
@@ -48,6 +48,67 @@ MODIFIED: ["file4", "file5", "file6"]
 DELETED: ["file7", "file8", "file9"]
 */
 
-// Git Add & Commit 
+// Git Add 
+export async function gitAdd(): Promise<void> {
+  // 1) View all added, modified and deleted items via git status 
+  // 2) Select which items to add to git 
+        // git add . -> for all items
+        // git add <item-name> -> for individual items (keep track of items to add in array, loop through and add) 
+  try {
+    const { stdout, stderr } = await exec('git status');
+    processGitStatusLog(stdout);
+  } catch (error) {
+      console.log('Error with git add');
+  }
+}
+
+function processGitStatusLog(gitStatus: string): void {
+  const fileLines = gitStatus.split('\n');
+  let untrackedFilesStarted = false;
+  const dontAddFromFile = ['(use "git add <file>..." to include in what will be committed)', 'no changes added to commit (use "git add" and/or "git commit -a")', 'Untracked files:', ''];
+  console.log(fileLines);
+
+  let data = {modified: [], deleted: [], untracked: []};
+  for (let line of fileLines) {
+    if (line.includes('modified:')) {
+      const fileNameTemp = line.split("modified:")[1];
+      const fileName = fileNameTemp.split('/')[fileNameTemp.split('/').length - 1];
+      console.log("MODIFICO: ", fileName);
+      data.modified.push(fileName);
+    } else if (line.includes('deleted:')) {
+      const fileNameTemp = line.split("deleted:")[1];
+      const fileName = fileNameTemp.split('/')[fileNameTemp.split('/').length -1];
+      console.log('DELETICO: ', fileName);
+      data.deleted.push(fileName);
+    } else {
+        let trimmedLine = line.trim();
+        if (trimmedLine === 'Untracked files:') {
+          untrackedFilesStarted = true;
+        }
+
+        if (untrackedFilesStarted) {
+          if (!(dontAddFromFile.includes(trimmedLine))) {
+            const fileName = trimmedLine.split('/')[trimmedLine.split('/').length -1];
+            data.untracked.push(fileName);
+          }
+        }
+    }
+  }
+
+  console.log(data);
+}
+
+//TRIM:  Untracked files:
+//TRIM:  (use "git add <file>..." to include in what will be committed)
+//TRIM:  .gitignore2
+//TRIM:  git-logs.txt
+//TRIM:  src/testAdd2.txt
+//TRIM:
+//TRIM:  no changes added to commit (use "git add" and/or "git commit -a")
+
+// Git Commit 
+// export async function gitCommit(): Promise<void> {
+//  return promise<null>;
+//}
 
 // Git Push (Local And Remote)
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-import { setupRepository, createGitignoreFile } from "./git-helpers/git-helpers";
+import { setupRepository, createGitignoreFile, gitAdd } from "./git-helpers/git-helpers";
 
-createGitignoreFile()
+// Works
+// createGitignoreFile()
+
+// Works
 // setupRepository();
+
+// Main Program Execution
+// ** Just for testing purposes -> will create routes to access these functions 
+function main(): void {
+  let isRepoSetup: boolean = false;
+  if (!isRepoSetup) {
+    createGitignoreFile();
+    setupRepository();
+    isRepoSetup = true;
+  }
+}
+
+gitAdd()
